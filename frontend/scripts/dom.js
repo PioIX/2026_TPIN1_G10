3c103a0dc6b47f26f7f1af8744ec75edc75dd37f
juaniTelmo <jtelmo@pioix.edu.ar>
2026-06-08 09:09:20 -0300

    login/registro iniciados

--- frontend/scripts/dom.js
+++ frontend/scripts/dom.js
@@ -1,1 +1,20 @@
+class UserInterface {
+    constructor() {
+    }
 
+    showModal(title, body) {
+        document.getElementById("modalTitle").textContent = title;
+        document.getElementById("modalBody").textContent = body;
+
+        const modalElement = document.getElementById("modal");
+
+        const modal = new bootstrap.Modal(modalElement, {
+            keyboard: true,
+            focus: true
+        });
+
+        modal.show();
+    }
+}
+
+const ui = new UserInterface();
--- frontend/scripts/logic.js
+++ frontend/scripts/logic.js
@@ -1,1 +1,37 @@
+function registrarUsuario(){
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
+    if(nombre === "" || email === "" || password === ""){
+        ui.showModal("Error", "Por favor, complete todos los campos.");
+        return;
+    }
+    if(!validateEmail(email)){
+        ui.showModal("Error", "Por favor, ingrese un correo electrónico válido.");
+        return;
+    }
+    if(password.length < 6){    
+        ui.showModal("Error", "La contraseña debe tener al menos 6 caracteres.");
+        return;
+    }
+    ui.showModal("Éxito", "Usuario registrado correctamente.");
+//agregado a bdd, cambio de pantala, etc.
+
+
+
+
+}
+
+function loginUsuario(){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if(email === "" || password === ""){
+        ui.showModal("Error", "Por favor, complete todos los campos.");
+        return;
+    }else{
+        ui.showModal("Éxito", "Inicio de sesión exitoso.");
+        //validar contra bdd, cambio de pantalla, etc.
+    }
+}
