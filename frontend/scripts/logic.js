c5b25ea4ae59cd3413d144dc15961b2f2a9f81ee
juaniTelmo <jtelmo@pioix.edu.ar>
2026-06-08 09:41:10 -0300

    prueba de agregado de fotos dinamico

--- frontend/scripts/logic.js
+++ frontend/scripts/logic.js
@@ -35,3 +35,18 @@ function loginUsuario(){
         //validar contra bdd, cambio de pantalla, etc.
     }
 }
+
+fetch("https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=carvajal")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("fotoJugador").src =
+      data.player[0].strCutout || data.player[0].strThumb;
+  });
+
+  
+fetch("https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=independiente")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("fotoEquipo").src =
+      data.teams[0].strBadge; // Escudo
+  })
