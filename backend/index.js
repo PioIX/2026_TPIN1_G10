da2ec707399c6f1e3c15bfea09550ff0591017db
joaquin1romero <jromero@pioix.edu.ar>
2026-06-11 08:01:19 -0300

    Pedidos get

--- backend/index.js
+++ backend/index.js
@@ -27,3 +27,92 @@ app.get('/', function(req, res){
  * res = response. Voy a responderle al cliente
  */
 
+app.get('/usuarios', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Usuarios WHERE id_usuarios=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Usuarios");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/datos', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Datos WHERE id_datos=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Datos");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/estadisticas', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Estadisticas WHERE id_estadisticas=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Estadisticas");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/jugadores', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Jugadores WHERE id_jugador=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Jugadores");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/trayectoria', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Trayectoria WHERE id_trayectoria=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Trayectoria");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/equipos', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Equipos WHERE id_equipos=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Equipos");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/jugadores', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Jugadores INNER JOIN Estadisticas on Estadisticas.id_jugador = Jugadores.id_jugador INNER JOIN Datos on Datos.id_jugador = Estadisticas.id_jugador WHERE Jugadores.id_jugador=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Jugadores INNER JOIN Estadisticas on Estadisticas.id_jugador = Jugadores.id_jugador INNER JOIN Datos on Datos.id_jugador = Estadisticas.id_jugador");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/jugadores', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Jugadores INNER JOIN Trayectoria on Trayectoria.id_jugador = Jugadores.id_jugador INNER JOIN Equipos on Equipos.id_equipo = Trayectoria.id_equipo WHERE Jugadores.id_jugador=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Jugadores INNER JOIN Trayectoria on Trayectoria.id_jugador = Jugadores.id_jugador INNER JOIN Equipos on Equipos.id_equipo = Trayectoria.id_equipo");
+    }    
+    res.send(respuesta);
+})
+
+app.get('/datos', async function(req,res){
+    let respuesta;
+    if (req.query.id != undefined) {
+        respuesta = await realizarQuery(`SELECT * FROM Datos INNER JOIN Estadisticas on Estadisticas.id_jugador = Datos.id_jugador INNER JOIN Jugadores on Jugadores.id_jugador = Datos.id_jugador INNER JOIN Trayectoria on Trayectoria.id_jugador = Jugadores.id_jugador INNER JOIN Equipos on Equipos.id_equipo = Trayectoria.id_equipo WHERE Jugadores.id_jugador=${req.query.id}`)
+    } else {
+        respuesta = await realizarQuery("SELECT * FROM Datos INNER JOIN Estadisticas on Estadisticas.id_jugador = Datos.id_jugador INNER JOIN Jugadores on Jugadores.id_jugador = Datos.id_jugador INNER JOIN Trayectoria on Trayectoria.id_jugador = Jugadores.id_jugador INNER JOIN Equipos on Equipos.id_equipo = Trayectoria.id_equipo");
+    }    
+    res.send(respuesta);
+})
