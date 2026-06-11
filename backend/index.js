49bb36795b70c1b0a6811eb380ab3185221e6a87
joaquin1romero <jromero@pioix.edu.ar>
2026-06-11 08:37:28 -0300

    Pedidos post 3/6

--- backend/index.js
+++ backend/index.js
@@ -116,3 +116,48 @@ app.get('/datos', async function(req,res){
     }    
     res.send(respuesta);
 })
+
+app.post('/jugadores', async function(req,res) {
+    console.log(req.body)
+    let jugadorExistente = await realizarQuery(`SELECT apellido FROM Jugadores WHERE apellido="${req.body.apellido}"`);
+    console.log(jugadorExistente)
+    if (jugadorExistente.length > 0) {
+        res.send("El jugador ya existe");
+    } else {
+        await realizarQuery(`
+        INSERT INTO Jugadores (pais,posicion,dorsal_equipo,dorsal_seleccion,valor_mercado,nombre,apellido,id_equipo) VALUES
+        ("${req.body.pais}","${req.body.poisicion}",${req.body.dorsal_equipo},${req.body.dorsal_seleccion},${req.body.valor_mercado},"${req.body.nombre}","${req.body.apellido}",${req.body.id_equipo});
+        `)
+        res.send("Jugador agregado")
+    }
+})
+
+app.post('/usuarios', async function(req,res) {
+    console.log(req.body)
+    let usuarioExistente = await realizarQuery(`SELECT email FROM Usuarios WHERE email="${req.body.email}"`);
+    console.log(usuarioExistente)
+    if (usuarioExistente.length > 0) {
+        res.send("El usuario ya existe");
+    } else {
+        await realizarQuery(`
+        INSERT INTO Usuarios (nombre_de_usuario,contrasena,email) VALUES
+        ("${req.body.nombre_de_usuario}","${req.body.contrasena}","${req.body.email}");
+        `)
+        res.send("Usuario registrado")
+    }
+})
+
+app.post('/datos', async function(req,res) {
+    console.log(req.body)
+    let datosExistentes = await realizarQuery(`SELECT id_jugador FROM Datos WHERE id_jugador=${req.body.id_jugador}`);
+    console.log(datosExistentes)
+    if (datosExistentes.length > 0) {
+        res.send("Los datos ya existen");
+    } else {
+        await realizarQuery(`
+        INSERT INTO Datos (edad,altura,peso,pierna_habil,id_jugador) VALUES
+        (${req.body.edad},${req.body.altura},${req.body.peso},"${req.body.pierna_habil}",${req.body.id_jugador});
+        `)
+        res.send("Datos agregados")
+    }
+})
